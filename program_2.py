84f2dc4f0ca5a840806bb9f396340950a9958b17
fossam1 <fossam1212@gmail.com>
2024-09-18 09:54:54 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,3 +1,7 @@
+# Samuel Foss
+# Program 2 Age Classifier
+# 9/18/2024
+
 # Write a program that asks the user to enter a person's age.  The program should display a message indicating whether the person is an infant, a child, a teenager, or an adult.  Following are the guidelines:
 
 # If the person is 1 year old or less, it should display "infant" (without quotes).
@@ -8,7 +12,13 @@
 def categorize_age(age):
     ageCategory = "TBD"
     ######################
-    # WRITE YOUR CODE HERE
+    if age <= 1 : ageCategory = "Infant"
+
+    if age > 1 and age < 13 : ageCategory = "Child"
+
+    if age >= 13 and age < 20 : ageCategory = "Teenager"
+
+    if age >= 20 : ageCategory = "Adult"
     ######################
 
 
@@ -16,7 +26,7 @@ def categorize_age(age):
 
 
 #### This piece of the code has been done for you,
-#### you only need to worry about the actual shipping 
+#### you only need to worry about the actual shipping
 #### charge logic in the weight_conversion function
 if __name__ == '__main__':
     # Local variables
@@ -25,3 +35,9 @@ if __name__ == '__main__':
     # Display the age
     ageBucket = categorize_age(age)
     print (ageBucket)
+    
+    #Output
+    #Enter the person's age: 14
+    #Teenager
+
+    #Process finished with exit code 0
